14b53d97857c887d7b91522257a78ee5dfe8fc19
secndK <jilmay02@gmail.com>
2024-10-31 15:47:52 +0000

    deuxieme version

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>accueil</title>
+
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+ <!-- La  Navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">CRUD PHP + Bootstrap</a>
+        </div>
+    </nav>
+
+
+    <!-- le tableau --> 
+
+
+    <table class="table table-hover text-center">
+  <thead class="table-pink">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
+      <th scope="col">Email</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Action</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>chief</td>
+        <td>keef</td>
+        <td>
+        <i class="bi bi-pencil-square"></i>
+        </td>
+
+
+
+
+
+
+
+    </tr>
+   
+   
+  </tbody>
+</table>
+
+<div class="d-flex justify-content-center">
+    <a href="add_new.php" class="btn btn-outline-secondary">Ajouter nouveau</a>
+</div>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
